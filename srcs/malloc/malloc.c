7949ada4b831f2672026facaff8d9f905763fdad
eliottdhommee <edhommee@e3r10p13.42.fr>
2019-10-22 17:19:27 +0200

    start malloc

--- srcs/malloc/malloc.c
+++ srcs/malloc/malloc.c
@@ -6,14 +6,38 @@
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 14:46:17 by edhommee          #+#    #+#             */
-/*   Updated: 2019/03/04 14:46:25 by edhommee         ###   ########.fr       */
+/*   Updated: 2019/10/22 17:17:55 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft_malloc.h>
 
+void		*new_page(int size)
+{
+	size_t	page_len;
+
+	if (size == 1)
+		page_len = TINY_ZONE;
+	else if (size == 2)
+		page_len = SMALL_ZONE;
+	else
+		page_len = size;
+	(t_page*)mmap(NULL, page_len, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+}
+
+int			get_size_type(size_t size)
+{
+	if (size <= TINY_MALLOC)
+		return(1);
+	else if (size <= SMALL_MALLOC)
+		return(2);
+	else
+		return(3);
+}
+
 void		*malloc(size_t size)
 {
-	size = 0;
-	return(0);
+	static void		**root_pages;;
+	if (size == 0)
+		return (NULL);
 }
--- srcs/malloc/page_new.c
+++ srcs/malloc/page_new.c
@@ -1,27 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_malloc.h                                     :+:      :+:    :+:   */
+/*   page_new.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/04 14:45:39 by edhommee          #+#    #+#             */
-/*   Updated: 2019/10/22 14:57:45 by edhommee         ###   ########.fr       */
+/*   Created: 2019/10/22 17:08:14 by edhommee          #+#    #+#             */
+/*   Updated: 2019/10/22 17:18:00 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_MALLOC_H
-# define LIBFT_MALLOC_H
+#include <libft_malloc.h>
 
-# include <stdlib.h>
-# include <sys/mman.h>
-# define TINY_MALLOC 100
-# define SMALL_MALLOC 1000
-# define LARGE_MALLOC 10000
 
-void		*malloc(size_t size);
-void		free(void *ptr);
-void		*realloc(void *ptr, size_t size);
-void		show_alloc_mem();
-
-#endif
