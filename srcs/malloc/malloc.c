55ead3a9f742af0ed0b2b7386271fb37c33275ba
Eliott D'HOMMEE <edhommee@e2r9p8.42.fr>
2021-02-12 12:35:15 +0100

    little clean

--- srcs/malloc/malloc.c
+++ srcs/malloc/malloc.c
@@ -6,7 +6,7 @@
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 14:46:17 by edhommee          #+#    #+#             */
-/*   Updated: 2021/02/12 12:13:40 by edhommee         ###   ########.fr       */
+/*   Updated: 2021/02/12 12:34:05 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,22 @@ void		*new_page(size_t size)
 	return (mmap(NULL, get_page_size(size), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0));
 }
 
+void		set_new_node(t_page *ptr, size_t size)
+{
+
+	t_page		*tmp;
+
+	tmp = ptr->next;
+	ptr->next = new_node(((char*)ptr + size), ptr->size - size - sizeof(t_page*), FALSE);
+	(ptr->next)->prev = ptr;
+	(ptr->next)->next = tmp;
+	ptr->size = size;
+}
+
 t_page		*search_free_space(t_page **root, size_t size)
 {
 	t_page		*tmp;
 	t_page		*tmp_prev;
-	t_page		*tmp_next;
 
 	tmp = *root;
 	while (tmp && tmp->free == FALSE && tmp->size <= size)
@@ -47,36 +58,22 @@ t_page		*search_free_space(t_page **root, size_t size)
 	}
 	if (tmp && tmp->size >= size && tmp->free == TRUE)
 	{
-		tmp->free = FALSE;
 		if (tmp->size - size > 2 * sizeof(t_page))
-		{
-			tmp_next = tmp->next;
-			tmp->next = new_node(((char*)tmp + size), tmp->size - size, FALSE);
-			(tmp->next)->prev = tmp;
-			(tmp->next)->next = tmp_next;
-			tmp->size = size;
-		}
+			set_new_node(tmp, size);
+		tmp->free = FALSE;
 	}
 	else if (tmp)
 	{
-		tmp->next = new_node(new_page(size), get_page_size(size), TRUE);
-		tmp_next = tmp->next;
-		tmp->next = new_node(((char*)tmp + size), tmp->size - size, FALSE);
-		(tmp->next)->prev = tmp;
-		(tmp->next)->next = tmp_next;
-		tmp->size = size;
+		tmp->next = new_node(new_page(size), get_page_size(size) - sizeof(t_page*), TRUE);
+		set_new_node(tmp, size);
 		tmp = tmp->next;
 		tmp->free = FALSE;
 	}
 	else
 	{
 		tmp = tmp_prev;
-		tmp->next = new_node(new_page(size), get_page_size(size), TRUE);
-		tmp_next = tmp->next;
-		tmp->next = new_node(((char*)tmp + size), tmp->size - size, FALSE);
-		(tmp->next)->prev = tmp;
-		(tmp->next)->next = tmp_next;
-		tmp->size = size;
+		tmp->next = new_node(new_page(size), get_page_size(size) - sizeof(t_page*), TRUE);
+		set_new_node(tmp, size);
 		tmp = tmp->next;
 		tmp->free = FALSE;
 	}
