34c40e4d54f65640278e0984ad3c4a6e204648fe
Eliott D'HOMMEE <edhommee@e2r8p8.42.fr>
2021-02-22 12:42:11 +0100

    plouf

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,28 +6,16 @@
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 14:46:47 by edhommee          #+#    #+#             */
-/*   Updated: 2021/02/16 08:10:31 by edhommee         ###   ########.fr       */
+/*   Updated: 2021/02/22 11:27:21 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft_malloc.h>
 
-char	*ft_strcpy(char *dest, const char *src)
-{
-	char		*tmp;
-
-	tmp = dest;
-	if (dest == src)
-		return (dest);
-	while (*src)
-		*dest++ = *src++;
-	*dest = '\0';
-	return (tmp);
-}
-
 void		*realloc(void *ptr, size_t size)
 {
 	char *tmp;
+ft_printf("%w%lu\n", 2, sizeof(t_page));
 
 	tmp = malloc(size);
 	tmp = ft_strcpy(tmp, ptr);
