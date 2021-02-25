1e4d9c1fb670b5cd9aaf22c0697b18f7557a5435
Eliott D'HOMMEE <edhommee@e2r8p8.42.fr>
2021-02-25 08:15:09 +0100

    printf pointer to fd=2

--- srcs/show_alloc_mem.c
+++ srcs/show_alloc_mem.c
@@ -6,12 +6,39 @@
 /*   By: edhommee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 10:58:52 by edhommee          #+#    #+#             */
-/*   Updated: 2021/02/24 11:45:42 by edhommee         ###   ########.fr       */
+/*   Updated: 2021/02/25 08:13:58 by edhommee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft_malloc.h>
 
-void		show_alloc_mem()
+#include <stdint.h>
+
+void		write_char(char c)
 {
+	write(2, &c, 1);
 }
+
+char		hex_digit(int v) {
+	if (v >= 0 && v < 10)
+		return '0' + v;
+	else
+		return 'a' + v - 10;
+}
+
+void		print_address_hex(void* p0) {
+	int i;
+	uintptr_t p = (uintptr_t)p0;
+
+	write_char('0');
+	write_char('x');
+	for(i = (sizeof(p) << 3) - 4; i>=0; i -= 4)
+	{
+		write_char(hex_digit((p >> i) & 0xf));
+	}
+	write_char('\n');
+}
+
+/*void		show_alloc_mem()
+{
+}*/
